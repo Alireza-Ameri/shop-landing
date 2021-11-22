58e43cb78734a32fb21149bed57a2e92f70a1837
Alireza Ameri <a.ameri1379@yahoo.com>
2021-11-22 19:34:19 +0330

    add Discover component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import react from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Carousel from "./Components/Carousel/Carousel";
+import Discover from "./Components/Discover/Discover";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Carousel />
+      <Discover />
     </div>
   );
 };
---
+++ src/Components/Discover/Discover.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Discover = () => {
+  return (
+    <div className="mt-60 flex ml-14 mb-7   ">
+      <div className="w-540 h-270 bg-gray-200 mx-6 relative">
+        <div className="h-8 w-269 ml-9 mt-9  mb-2 ">
+          <span className="text-2xl">Luxurious Eyewear </span>
+        </div>
+        <div className="h-12 w-345 ml-9 mt-3   ">
+          <span className="text-base text-gray-500">
+            See the beauty of exotic world with the luxurious glasses
+          </span>
+        </div>
+        <div className="h-7 w-28 ml-11  mt-3">
+          <span className="text-base">Discover Now</span>
+        </div>
+        <div className="w-238 h-114 bg-black flex justify-center items-center  absolute right-0 bottom-0 ">
+          <span className="text-white text-2xl"> Photo</span>
+          <img src=""></img>
+        </div>
+      </div>
+      <div className="w-540 h-270 bg-gray-200 mx-6  relative">
+        <div className="h-8 w-269 ml-9 mt-9  mb-2 ">
+          <span className="text-2xl">Comfortable Watches </span>
+        </div>
+        <div className="h-12 w-345 ml-9 mt-3  ">
+          <span className="text-base text-gray-500">
+            Feels the balancing function and beauty in our wooden watches
+          </span>
+        </div>
+        <div className="h-7 w-28 ml-11  mt-3 ">
+          <span className="text-base">Discover Now</span>
+        </div>
+        <div className="w-238 h-114 bg-black flex justify-center items-center  absolute right-0 bottom-0 ">
+          <span className="text-white text-2xl"> Photo</span>
+          <img src=""></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
